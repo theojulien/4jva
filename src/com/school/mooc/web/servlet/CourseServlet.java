78f8e7158a4564174a0fcc74b1655e1e3aa22e55
théo julien <162090@supinfo.com>
2014-06-06 14:02:35 +0200

    test suppression foreach

--- src/com/school/mooc/web/servlet/CourseServlet.java
+++ src/com/school/mooc/web/servlet/CourseServlet.java
@@ -19,7 +19,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.jasper.tagplugins.jstl.ForEach;
 
 /**
  *
